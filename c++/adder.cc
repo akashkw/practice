fbb461dd1a1311378c1de3fa9f13d43cc56ffd95
Akash Kwatra <akashkw@gmail.com>
2018-06-25 23:55:54 -0500

    implemented adder

---
+++ c++/adder.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int a;
+int b;
+
+int main(){
+    cout<<"Enter first number:\n";
+    cin >> a;
+    cout<<"Enter second number\n";
+    cin>>b;
+    cout<<"Result is "<<(a+b)<<endl;
+    return 0;
+}
