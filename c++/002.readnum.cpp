4bfbd5362ca6e04ed682db154e4f703f8fde66aa
Akash Kwatra <akashkw@gmail.com>
2018-06-30 12:50:59 -0500

    fix formatting and write readnum.cpp

---
+++ c++/002.readnum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Please enter a number" << endl;
+    cin >> num;
+    cout << "Your number is " << num << endl;
+    return 0;
+}
+
