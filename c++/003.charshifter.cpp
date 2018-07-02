3023ad3de40d3fea89a46efec78f56706445813a
Akash Kwatra <akashkw@gmail.com>
2018-07-01 19:34:36 -0500

    implement charshifter to test casting

---
+++ c++/003.charshifter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 003
+// Test of implicit cast of char to int and explicit cast back to char
+// Reads in number and character, prints out character shifted by number
+
+int main() {
+    int n;
+    char c;
+    cout << "Please enter a character" << endl;
+    cin >> c;
+    cout << "Please enter how much to shift it by" << endl;
+    cin >> n;
+    cout << "Your character is " << char(n + c) << endl;
+    return 0;
+}
+
