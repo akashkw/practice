31b200a81843baac4feaca35f435289a0247ffa8
Akash Kwatra <akashkw@gmail.com>
2018-07-03 22:00:47 -0500

    add size checker

---
+++ c++/004/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// 004
+// Size Checker
+// Check the size in bytes of multiple datatypes
+// Prints out the data type and its size
+
+int main() {
+    cout << endl << "004 - Size Checker" << endl << endl;
+
+    cout << "Size of char is " << sizeof(char) << " bytes" << endl;
+    cout << "Size of int is " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float is " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double is " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
+
