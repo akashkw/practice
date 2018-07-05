64a5c2048f4b0e103baea5ab40fc62954db07f22
Akash Kwatra <akashkw@gmail.com>
2018-07-04 22:50:12 -0500

    add cmath tests

---
+++ c++/005/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// 005
+// cmath
+// Play around with cmath functions in cmath library
+// Prints out various math functions and a use for each
+
+int main() {
+    cout << endl << "005 - cmath" << endl << endl;
+
+    cout << "ceil(4.8) = " << ceil(4.8) << " and ceil(6.1) = " << ceil(6.1) << endl;
+    cout << "fabs(4.8) = " << fabs(4.8) << " and fabs(-6.1) = " << fabs(-6.1) << endl;
+    cout << "fmin(4.8, 6.1) = " << fmin(4.8, 6.1) << " and fmax(4.8, 6.1) = " << fmax(4.8, 6.1) << endl;
+    cout << "pow(2, 3) = " << pow(2, 3) << " and pow(3, 3) = " << pow(3, 3) << endl;
+
+    return 0;
+}
+
