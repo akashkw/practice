478b63bd5a5f237c6f246f8e58e05f7b113c73ee
Akash Kwatra <akashkw@gmail.com>
2018-07-06 00:26:28 -0500

    implement even odd

---
+++ c++/006/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// 006
+// even odd
+// Test out if statements and modulus
+// Takes in a number, prints out if even or odd
+
+int main() {
+    cout << endl << "006 - even odd" << endl << endl;
+
+    int n;
+    cout << "Please enter a number :: ";
+    cin >> n;
+    cout << endl;
+    if ( n%2 == 0 ){
+        cout << n << " is even";
+    }
+    else {
+        cout << n << " is odd";
+    }
+    cout << endl << endl;
+    return 0;
+}
+
