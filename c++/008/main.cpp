e4a14c8c7f8cf7cfed69450fd1a8c015361e7361
Akash Kwatra <akashkw@gmail.com>
2018-07-11 13:43:23 -0500

    ternary even odd

---
+++ c++/008/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// 008
+// ternary even odd
+// Test out ternary operators
+// Takes in a number, prints out if even or odd, uses ternary operators
+
+int main() {
+    cout << endl << "008 - ternary even odd" << endl << endl;
+
+    int n;
+    cout << "Please enter a number :: ";
+    cin >> n;
+    cout << endl;
+    ( n%2 == 0 ) ? cout << n << " is even" : cout << n << " is odd";
+    cout << endl << endl;
+    return 0;
+}
+
