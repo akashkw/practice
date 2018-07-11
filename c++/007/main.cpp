964bc15e693e6c8cbe87ecf52a29429b4a89002a
Akash Kwatra <akashkw@gmail.com>
2018-07-11 11:35:49 -0500

    grade checker to test switch statements

---
+++ c++/007/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// 007
+// grade checker
+// Test out swtich statements
+// Takes in a character as a grade letter, prints out meaning
+
+int main() {
+    cout << endl << "007 - grade checker" << endl << endl;
+
+    char c;
+    cout << "Please enter your grade :: ";
+    cin >> c;
+    cout << endl;
+    switch(c){
+        case 'A': case 'a':
+            cout << "You did excellent";
+            break;
+        case 'B': case 'b':
+            cout << "Pretty good";
+            break;
+        case 'C': case 'c':
+            cout << "Just barely made it";
+            break;
+        case 'F': case 'f':
+            cout << "You failed";
+            break;
+        default:
+            cout << "That's not a valid grade letter";
+    }
+    cout << endl << endl;
+    return 0;
+}
+
