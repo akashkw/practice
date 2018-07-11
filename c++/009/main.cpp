5688e37fe0d66960741d8c9a8c6370ed11f766e1
Akash Kwatra <akashkw@gmail.com>
2018-07-11 17:33:11 -0500

    loop testing

---
+++ c++/009/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// 009
+// loop test
+// Test out for and while loops
+// Takes in a number n, prints out 1 through n using for and while loops
+
+int main() {
+    cout << endl << "009 - loop test" << endl << endl;
+
+    int n;
+    cout << "Please enter a number :: ";
+    cin >> n;
+    cout << endl << endl;
+    
+    cout << "For Loop Test" << endl << endl;
+    for(int i = 0; i <= n; i++){
+        cout << i << endl;
+    }
+    cout << endl;
+    
+    cout << "While Loop Test" << endl << endl;
+    int w = 0;
+    while(w <= n){
+        cout << w << endl;
+        w++;
+    }
+
+    cout << endl;
+    return 0;
+}
+
