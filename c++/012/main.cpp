f63c1947b8f9ac52891b46913b79b97499b471d1
Akash Kwatra <akashkw@gmail.com>
2018-07-13 16:57:51 -0500

    print all three methods of invocation

--- c++/012/main.cpp
+++ c++/012/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <ctime>
+#include <chrono>
 
 using namespace std;
 
 // 012
 // inline sum
-// Test out inline functions, as well as ctime
+// Test out inline functions, as well as chrono
 // Takes in a number, calculates sum with and without an inline function, reports times
 
-inline int sum(int x, int y){
+int sum(int x, int y){
+    return x + y;
+}
+
+inline int inline_sum(int x, int y){
     return x + y;
 }
 
 int main() {
     cout << endl << "012 - inline sum" << endl << endl;
+
+    // TODO implement timer using chrono
     
     int x, y;
     cout << "Please enter first number :: ";
@@ -21,7 +27,9 @@ int main() {
     cout << "Please enter second number :: ";
     cin >> y;
 
-    cout << endl << "Sum is " << (x + y) << endl;
+    cout << endl << "Expression :: sum is " << (x + y) << endl;
+    cout << endl << "Inline Function :: sum is " << inline_sum(x, y) << endl;
+    cout << endl << "Function ::  sum is " << sum(x, y) << endl;
     cout << endl;
     return 0;
 }
