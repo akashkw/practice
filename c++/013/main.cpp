40a3c5b775d97ddd21e6b50bf9d479bb628b97ec
Akash Kwatra <akashkw@gmail.com>
2018-07-14 12:21:08 -0500

    pointer practice

---
+++ c++/013/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// 013
+// make pointer
+// Test out pointer initialization
+// Takes in a number, creates a pointer to it
+
+int main() {
+    cout << endl << "013 - make pointer" << endl << endl;
+    
+    int x;
+    cout << "Please enter a number (x) :: ";
+    cin >> x;
+    
+    cout << endl << "Creating pointer (p) to your number (x)" << endl << endl;
+
+    cout << "Size of int x is " << sizeof(int) << " bytes" << endl;
+    cout << "Size of int *p is " << sizeof(int *) << " bytes" << endl;
+    cout << endl;
+
+    int *p = &x;
+
+    cout << "x = " << x << endl;
+    cout << "&x = " << &x << endl;
+    cout << endl;
+    cout << "p = " << p << endl;
+    cout << "*p = " << *p << endl;
+    cout << "&p = " << &p << endl;
+    cout << endl;
+    cout << "*&p = " << *&p << endl;
+    cout << "**&p = " << **&p << endl;
+
+    cout << endl;
+    return 0;
+}
+
