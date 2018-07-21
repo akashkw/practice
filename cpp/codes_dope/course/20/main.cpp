110707c29ff7a100f1ae1bab711878623320d5b4
akashkw <akashkw@gmail.com>
2018-07-21 13:54:24 -0500

    basic define practice

---
+++ cpp/codes_dope/course/20/main.cpp
@@ -0,0 +1,35 @@
+// Copyright 2019 Akash Kwatra
+
+#include <iostream>
+
+#define PI 3.14
+#define average(a, b) ((a + b) / 2)
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// 20
+// basic define
+// Test out some simple define statements
+// Print out PI, read in two numbers and print average
+
+int main() {
+    cout << endl << "20 - basic define" << endl << endl;
+
+    cout << "PI = " << PI << endl;
+    cout << endl;
+
+    int a, b;
+    cout << "Please enter first number :: ";
+    cin >> a;
+    cout << "Please enter seond number :: ";
+    cin >> b;
+    cout << endl;
+
+    cout << "average(a, b) = " << average(a, b) << endl;
+
+    cout << endl;
+    return 0;
+}
+
