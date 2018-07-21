e88a45a425379e0566372c3b68f530942ce2b4d0
akashkw <akashkw@gmail.com>
2018-07-21 02:23:50 -0500

    std string practice

---
+++ cpp/codes_dope/course/19/main.cpp
@@ -0,0 +1,50 @@
+// Copyright 2019 Akash Kwatra
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+
+// 019
+// std strings
+// Test out standard cpp strings
+// Read in name, perform various operations on strings
+
+int main() {
+    cout << endl << "019 - std strings" << endl << endl;
+
+    cout << "Please enter your name :: ";
+    string name;
+    getline(cin, name);
+    cout << "name = " << name << endl;
+    cout << endl;
+    cout << "name.length() = " << name.length() << endl;
+    cout << "name.size() = " << name.size() << endl;
+    name.resize(name.size() + 4, '*');
+    cout << "name.resize(name.size() + 4, '*') = " << name << endl;
+    cout << endl;
+    cout << "name.empty() = " << name.empty() << endl;
+    cout << "name.clear()" << endl;
+    name.clear();
+    cout << "name.empty() = " << name.empty() << endl;
+    cout << endl;
+    cout << "name = \"testing\"" << endl;
+    name = "testing";
+    cout << "name[4] = " << name[4] << endl;
+    cout << "name.at(4) = " << name.at(4) << endl;
+    cout << endl;
+    cout << "name.compare(string(\"hello\")) = "
+         << name.compare(string("hello")) << endl;
+    cout << "name.compare(string(\"testing\")) = "
+         << name.compare(string("testing")) << endl;
+    cout << endl;
+    cout << "name.find('i') = " << name.find('i') << endl;
+    cout << "name.substr(2, 5) = " << name.substr(2, 4) << endl;
+    cout << endl;
+    return 0;
+}
+
