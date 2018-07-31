efbd600ee070fab148aec742cc485673d9c22f8c
akashkw <akashkw@gmail.com>
2018-07-31 01:20:44 -0500

    isunique in 3 ways cpp

--- cpp/ctci/isunique.cpp
+++ cpp/ctci/isunique.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <string>
+#include <unordered_set>
+#include <bitset>
+#include <algorithm>
 using namespace std;
     
-int main() {
+bool isunique_hash(const string &str) {
+    unordered_set<char> hashset;
+    for(const char &c : str) {
+        if(hashset.find(c) != hashset.end()) {
+            return false;
+        }
+        else {
+            hashset.insert(c);
+        }
+    }
+    return true;
+}
 
+bool isunique_bits(const string &str) {
+    bitset<256> ascii;
+    for(const char &c : str) {
+        if (ascii.test(c)) {
+            return false;
+        }
+        else {
+            ascii.set(c);
+        }
+    }
+    return true;
+}
+
+bool isunique_sort(const string &str) {
+    string strcpy = str.c_str();
+    sort(strcpy.begin(), strcpy.end());
+    for (int i = 0; i < strcpy.size() - 1; i++) {
+        if (strcpy[i] == strcpy[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+   string in;
+   getline(cin, in);
+   while(!in.empty()) {
+        cout << in << " is unique? :: " << boolalpha << isunique_hash(in) << endl;
+        cout << in << " is unique? :: " << boolalpha << isunique_bits(in) << endl;
+        cout << in << " is unique? :: " << boolalpha << isunique_sort(in) << endl;
+        cout << endl;
+        getline(cin, in);
+   }
 }
