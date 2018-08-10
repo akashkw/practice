22c0e9c9357d8791e9d65c97e787226d739c6277
Akash Kwatra <akashkw@gmail.com>
2018-08-09 19:19:33 -0500

    fix bugs and implement test cases

--- python/misc/quicksort.py
+++ python/misc/quicksort.py
@@ -3,9 +3,16 @@ def quicksort(lst):
         return lst
     smaller = [x for x in lst[1:] if x <= lst[0]]
     larger = [x for x in lst[1:] if x > lst[0]]
-    return quicksort(smaller) + lst[0] + quicksort(larger)
+    return quicksort(smaller) + [lst[0]] + quicksort(larger)
 
 if __name__ == '__main__':
     l = [1,5,7,3,5,9,3]
-    quicksort(l)
     print(l)
+    print(quicksort(l))
+    l = list(range(15))
+    l.reverse()
+    print(l)
+    print(quicksort(l))
+    l = [4,67,13,6,2,565,2,3,-4]
+    print(l)
+    print(quicksort(l))
