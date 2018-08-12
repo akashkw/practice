445e76a542748ff24ac28e3871bad299ea169e54
akashkw <akashkw@gmail.com>
2018-08-12 00:10:09 -0500

    implement bfs

--- cpp/algos_ds/vanilla_ps1/vanilla_ps1.cpp
+++ cpp/algos_ds/vanilla_ps1/vanilla_ps1.cpp
@@ -12,8 +12,8 @@ public:
         delete[] adjacent;
     }
     void add_edge(int node_one, int node_two) {
-        adjacent[node_one - 1].push_back(node_two);
-        adjacent[node_two - 1].push_back(node_one);
+        adjacent[node_one].push_back(node_two);
+        adjacent[node_two].push_back(node_one);
     }
     void print_graph() {
         for(int i = 0; i < num_nodes; i++) {
@@ -23,6 +23,61 @@ public:
             }
             std::cout << "*" << std::endl;
         }
+    }
+    int connected_components() {
+        bool* visited = new bool[num_nodes];
+        for(int i = 0; i < num_nodes; i++) {
+            visited[i] = false;
+        }
+        std::list<int> queue;
+        int components = 0;
+        for(int i = 0; i < num_nodes; i++) {
+            if(!adjacent[i].empty() && !visited[i]) {
+                visited[i] = true;
+                queue.push_back(i);
+                components++;
+            }
+            while(!queue.empty()) {
+                int node = queue.front();
+                queue.pop_front();
+                for(int adj_node : adjacent[node]) {
+                    if(!visited[adj_node]) {
+                        visited[adj_node] = true;
+                        queue.push_back(adj_node);
+                    }
+                }
+            }
+        }
+        delete visited;
+        return components;
+    }
+
+    int nodes_reachable(int start_node, int furthest_dist) {
+        bool* visited = new bool[num_nodes];
+        for(int i = 0; i < num_nodes; i++) {
+            visited[i] = false;
+        }
+        std::list<int> queue;
+        int node_count = 1;
+        int path_length = 0;
+        visited[start_node] = true;
+        queue.push_back(start_node);
+        
+        while(!queue.empty() && node_count <= furthest_dist) {
+            int node = queue.front();
+            queue.pop_front();
+            for(int adj_node : adjacent[node]) {
+                if(!visited[adj_node]) {
+                    visited[adj_node] = true;
+                    queue.push_back(adj_node);
+                    node_count++;
+                }
+            }
+            path_length++;
+        }
+
+        delete visited;
+        return node_count;
     }
 };
 
@@ -37,14 +92,15 @@ int main() {
         std::cin >> start_node;
         std::cin >> furthest_dist;
     
-        Graph graph = num_nodes;
+        Graph* graph = new Graph(num_nodes);
         for(int j = 0; j < num_edges; j++) {
             int node_one, node_two;
             std::cin >> node_one;
             std::cin >> node_two;
-            graph.add_edge(node_one, node_two);
+            graph->add_edge(node_one - 1, node_two - 1);
         }
-
+        std::cout << graph->connected_components() << " " << graph->nodes_reachable(start_node - 1, furthest_dist) << std::endl;
+        delete graph;
     }
 
 }
