175ca5cccb6270789c83a79997297c76bdf8e6b6
akashkw <akashkw@gmail.com>
2018-10-13 00:22:42 -0500

    optimize two buttons in python

--- python/codeforces/two_buttons/two_buttons.py
+++ python/codeforces/two_buttons/two_buttons.py
@@ -12,9 +12,9 @@ def bfs(start, finish):
         visited.add(val)
         if val == finish:
             return level
-        if val - 1 not in visited:
+        if val - 1 > -1 and val - 1 not in visited:
             queue.appendleft((level + 1, val - 1))
-        if val * 2 not in visited:
+        if val * 2 < finish + 2 and val * 2 not in visited:
             queue.appendleft((level + 1, val * 2))
 
 if __name__ == '__main__':
