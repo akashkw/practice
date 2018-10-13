f825e8ca79fa258af9acc666a6d383fdd374f527
akashkw <akashkw@gmail.com>
2018-10-12 23:19:51 -0500

    update two buttons to use bfs

--- python/codeforces/two_buttons/two_buttons.py
+++ python/codeforces/two_buttons/two_buttons.py
@@ -1,17 +1,16 @@
+from collections import deque
+
+def bfs(start, finish):
+    queue = deque()
+    queue.appendleft((0, start))
+    while queue:
+        level, val = queue.pop()
+        if val == finish:
+            return level
+        queue.appendleft((level + 1, val - 1))
+        queue.appendleft((level + 1, val * 2))
 
 if __name__ == '__main__':
     count = 0
     start, finish = tuple(map(int, input().split()))
-    if start > finish:
-        count = start - finish
-    else:
-        while start * 2 > finish:
-            start -= 1
-            count += 1
-        while start < finish:
-            start *= 2
-            count += 1
-        while start != finish:
-            start -= 1
-            count += 1
-    print(count)
+    print(bfs(start, finish))
