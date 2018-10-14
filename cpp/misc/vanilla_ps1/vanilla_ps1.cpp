546457135a569f7417d823ef36aab176aeff7f32
Akash Kwatra <akashkw@gmail.com>
2018-10-14 02:13:21 -0500

    optimize vanilla c++ for speed and readability

--- cpp/misc/vanilla_ps1/vanilla_ps1.cpp
+++ cpp/misc/vanilla_ps1/vanilla_ps1.cpp
@@ -1,106 +1,54 @@
 #include <iostream>
-#include <list>
-
-class Graph {
-    int num_nodes;
-    std::list<int>* adjacent;
-public:
-    Graph(int nodes) : num_nodes(nodes) {
-        adjacent = new std::list<int>[num_nodes];
-    }
-    ~Graph() {
-        delete[] adjacent;
-    }
-    void add_edge(int node_one, int node_two) {
-        adjacent[node_one].push_back(node_two);
-        adjacent[node_two].push_back(node_one);
-    }
-    void print_graph() {
-        for(int i = 0; i < num_nodes; i++) {
-            std::cout << "[" << i+1 << "]->";
-            for(int num : adjacent[i]) {
-                std::cout << num << "->";
-            }
-            std::cout << "*" << std::endl;
-        }
-    }
-    int connected_components() {
-        bool* visited = new bool[num_nodes];
-        for(int i = 0; i < num_nodes; i++) {
-            visited[i] = false;
-        }
-        std::list<int> queue;
-        int components = 0;
-        for(int i = 0; i < num_nodes; i++) {
-            if(!adjacent[i].empty() && !visited[i]) {
-                visited[i] = true;
-                queue.push_back(i);
-                components++;
-            }
-            while(!queue.empty()) {
-                int node = queue.front();
-                queue.pop_front();
-                for(int adj_node : adjacent[node]) {
-                    if(!visited[adj_node]) {
-                        visited[adj_node] = true;
-                        queue.push_back(adj_node);
-                    }
-                }
-            }
-        }
-        delete[] visited;
-        return components;
-    }
-
-    int nodes_reachable(int start_node, int furthest_dist) {
-        bool* visited = new bool[num_nodes];
-        for(int i = 0; i < num_nodes; i++) {
-            visited[i] = false;
-        }
-        std::list<int> queue;
-        int node_count = 1;
-        int path_length = 0;
-        visited[start_node] = true;
-        queue.push_back(start_node);
-        
-        while(!queue.empty() && node_count <= furthest_dist) {
-            int node = queue.front();
-            queue.pop_front();
-            for(int adj_node : adjacent[node]) {
-                if(!visited[adj_node]) {
-                    visited[adj_node] = true;
-                    queue.push_back(adj_node);
-                    node_count++;
-                }
-            }
-            path_length++;
-        }
-
-        delete[] visited;
-        return node_count;
-    }
-};
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <algorithm>
+using namespace std;
 
 int main() {
-
-    int num_tests;
-    std::cin >> num_tests;
-    for(int i = 0; i < num_tests; i++) {
-        int num_nodes, num_edges, start_node, furthest_dist; 
-        std::cin >> num_nodes;
-        std::cin >> num_edges;
-        std::cin >> start_node;
-        std::cin >> furthest_dist;
-    
-        Graph* graph = new Graph(num_nodes);
-        for(int j = 0; j < num_edges; j++) {
-            int node_one, node_two;
-            std::cin >> node_one;
-            std::cin >> node_two;
-            graph->add_edge(node_one - 1, node_two - 1);
+    int tests;
+    cin >> tests;
+    while(tests--) {
+        int nodes, edges, start, distance;
+        scanf("%d %d %d %d", &nodes, &edges, &start, &distance);
+        start--;
+        vector<vector<int>> graph = vector<vector<int>>(nodes, vector<int>());
+        for(int i = 0; i < edges; i++) {
+           int left, right;
+           cin >> left >> right;
+           left--; right--;
+           graph[left].push_back(right);
+           graph[right].push_back(left);
         }
-        std::cout << graph->connected_components() << " " << graph->nodes_reachable(start_node - 1, furthest_dist) << std::endl;
-        delete graph;
+        int components = 0;
+        vector<bool> visited = vector<bool>(nodes, false);
+        queue<int> q1;
+        while(find(begin(visited), end(visited), false) != end(visited)) {
+            components++;
+            q1.push(find(begin(visited), end(visited), false) - begin(visited));
+            while(!q1.empty()) {
+                int node = q1.front(); q1.pop();
+                visited[node] = true;
+                for(const int &adj_node : graph[node]) {
+                    if(!visited[adj_node]) q1.push(adj_node);
+                }
+            }
+        }
+        visited = vector<bool>(nodes, false);
+        queue<pair<int, int>> q2;
+        int num_reachable_nodes = 0;
+        q2.push(make_pair(start, 0));
+        while(!q2.empty()) {
+            int node, level; tie(node, level) = q2.front(); q2.pop();
+            if(level > distance) break;
+            if(visited[node]) continue;
+            num_reachable_nodes++;
+            visited[node] = true;
+            for(const int adj_node : graph[node]) {
+                q2.push(make_pair(adj_node, level+1));
+            }
+        }
+        
+        printf("%d %d\n", components, num_reachable_nodes);
     }
-
 }
