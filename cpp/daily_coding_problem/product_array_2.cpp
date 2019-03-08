5e3a86ea1bfa1c1e0e4903dbd0e62c30442fec9c
Akash Kwatra <akashkw@gmail.com>
2019-03-08 17:31:24 -0600

    bonus not working

--- cpp/daily_coding_problem/product_array_2.cpp
+++ cpp/daily_coding_problem/product_array_2.cpp
@@ -34,6 +34,29 @@ vector<int> product_array(const vector<int> &vec) {
     return solution;
 }
 
+vector<int> bonus_product_array(const vector<int> &vec) {
+    if(vec.size() == 2) {
+        return {vec[1], vec[0]};
+    }
+    vector<int> left_array(vec.size(), 1);
+    vector<int> right_array(vec.size(), 1);
+    right_array[right_array.size() - 2] = vec[right_array.size()-1];
+    left_array[1] = vec[0];
+    for(int i = (int)right_array.size()-3; i >= 0; --i) {
+        right_array[i] = right_array[i+1] * right_array[i+2];
+    }
+    for(size_t i = 2; i <= left_array.size()-1; ++i) {
+        left_array[i] = left_array[i-1] * right_array[i-2];
+    }
+    vector<int> solution(vec.size());
+    solution[0] = right_array[1];
+    solution[solution.size()-1] = left_array[solution.size()-2];
+    for(size_t i = 1; i < vec.size() - 1; ++i) {
+        solution[i] = left_array[i-1] * right_array[i+1];
+    }
+    return solution;
+}
+
 TEST(SolutionFixture, test_1) {
     vector<int> vec = product_array({1,2,3,4,5});
     vector<int> solution = {120,60,40,30,24};
@@ -52,5 +75,35 @@ TEST(SolutionFixture, test_3) {
 TEST(SolutionFixture, test_4) {
     vector<int> vec = product_array({-3,1,-8,3});
     vector<int> solution = {-24,72,-9,24};
+    ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
+}
+TEST(SolutionFixture, test_5) {
+    vector<int> vec = product_array({-3,1});
+    vector<int> solution = {1,-3};
+    ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
+}
+TEST(BonusSolutionFixture, test_1) {
+    vector<int> vec = bonus_product_array({1,2,3,4,5});
+    vector<int> solution = {120,60,40,30,24};
+    ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
+}
+TEST(BonusSolutionFixture, test_2) {
+    vector<int> vec = bonus_product_array({3,2,1});
+    vector<int> solution = {2,3,6};
+    ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
+}
+TEST(BonusSolutionFixture, test_3) {
+    vector<int> vec = bonus_product_array({-3,2,1});
+    vector<int> solution = {2,-3,-6};
+    ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
+}
+TEST(BonusSolutionFixture, test_4) {
+    vector<int> vec = bonus_product_array({-3,1,-8,3});
+    vector<int> solution = {-24,72,-9,24};
+    ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
+}
+TEST(BonusSolutionFixture, test_5) {
+    vector<int> vec = bonus_product_array({-3,1});
+    vector<int> solution = {1,-3};
     ASSERT_TRUE(equal(begin(vec), end(vec), begin(solution)));
 }
