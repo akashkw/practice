0c8f196de37d1cb6cf3471b4414a097b5a0da852
Akash Kwatra <akashkw@gmail.com>
2019-03-16 16:22:55 -0500

    initial serialize commit

--- cpp/daily_coding_problem/3_serialize_tree.cpp
+++ cpp/daily_coding_problem/3_serialize_tree.cpp
@@ -2,9 +2,13 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <queue>
+#include <utility>
 #include <algorithm>
 
 #include "gtest/gtest.h"
+
+#include "test/structures.h"
 
 using namespace std;
 
@@ -29,11 +33,33 @@ using namespace std;
  */
 
 // Solution Function
-bool solution(int num) {
-    return num;
+template <typename T>
+string serialize(const node<T>* root) {
+    ostringstream str, tree;
+    queue<pair<int, const node<T>*>> q;
+    int depth = 0;
+    q.push(make_pair(1, root));
+
+    while(!q.empty()) {
+        int level = q.front().first;
+        const node<T>* this_node= q.front().second;
+        q.pop();
+        if(level > depth) depth = level;
+        tree << this_node->data << ",";
+        if(this_node->left != nullptr) {
+            q.push(make_pair(level+1, this_node->left)); 
+        }
+        if(this_node->right != nullptr) {
+            q.push(make_pair(level+1, this_node->right)); 
+        }
+    }
+    str << depth << " " << tree.str();
+    return str.str();
 }
 
 TEST(SolutionFixture, test_1) {
-    ASSERT_TRUE(solution(1));
+    node<int>* root = new node<int>(1, new node<int>(1), new node<int>(2));
+    cout << root << endl;
+    ASSERT_EQ(serialize(root), "2 1,1,2,");
 }
 
