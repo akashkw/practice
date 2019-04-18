56e618c8c084cd02713aa603374fba675e4c9967
akashkw <akashkw@gmail.com>
2019-04-17 23:34:20 -0500

    update some commets

--- daily_coding_problem/5_pair_car_cdr.py
+++ daily_coding_problem/5_pair_car_cdr.py
@@ -4,8 +4,6 @@ import pdb
 '''
 Problem Statement
 
-Good morning! Here's your coding interview problem for today.
-
 This problem was asked by Jane Street.
 
 cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
