e50088c9926407099605b914c8c593be4748a051
akashkw <akashkw@gmail.com>
2019-04-21 15:03:23 -0500

    unival unit tests

--- daily_coding_problem/8_count_unival_subtrees.cpp
+++ daily_coding_problem/8_count_unival_subtrees.cpp
@@ -40,4 +40,44 @@ TEST(SolutionFixture, test_1) {
     ASSERT_EQ(count_unival_subtrees(root), 1);
     delete root;
 }
+TEST(SolutionFixture, test_2) {
+    node<int>* root = new node<int>(5);
+    root->left = new node<int>(5);
+    root->right = new node<int>(5);
+    ASSERT_EQ(count_unival_subtrees(root), 3);
+    delete root;
+}
 
+TEST(SolutionFixture, test_3) {
+    node<int>* root = new node<int>(0);
+    root->left = new node<int>(1);
+    root->right = new node<int>(0);
+    root->right->left = new node<int>(1);
+    root->right->right = new node<int>(0);
+    root->right->left->left = new node<int>(1);
+    root->right->left->right = new node<int>(1);
+    ASSERT_EQ(count_unival_subtrees(root), 5);
+    delete root;
+}
+
+TEST(SolutionFixture, test_4) {
+    node<int>* root = new node<int>(5);
+    root->left = new node<int>(5);
+    root->right = new node<int>(5);
+    root->left->left = new node<int>(5);
+    root->left->right = new node<int>(5);
+    root->right->right = new node<int>(5);
+    ASSERT_EQ(count_unival_subtrees(root), 6);
+    delete root;
+}
+
+TEST(SolutionFixture, test_5) {
+    node<int>* root = new node<int>(5);
+    root->left = new node<int>(1);
+    root->right = new node<int>(5);
+    root->left->left = new node<int>(5);
+    root->left->right = new node<int>(5);
+    root->right->right = new node<int>(5);
+    ASSERT_EQ(count_unival_subtrees(root), 4);
+    delete root;
+}
