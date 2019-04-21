fc1c5ce8cfb6bdae476e22f256454b649c0b51a4
Akash Kwatra <akashkw@gmail.com>
2019-04-21 12:41:45 -0500

    small chages

--- daily_coding_problem/7_count_decodings.cpp
+++ daily_coding_problem/7_count_decodings.cpp
@@ -21,11 +21,31 @@ using namespace std;
  */
 
 // Solution Function
-bool solution(int num) {
-    return num;
+int count_decodings(const string &str) {
+    return 0;
 }
 
 TEST(SolutionFixture, test_1) {
-    ASSERT_TRUE(solution(1));
+    string encoded = "1";
+    ASSERT_EQ(count_decodings(encoded), 1);
 }
 
+TEST(SolutionFixture, test_2) {
+    string encoded = "111";
+    ASSERT_EQ(count_decodings(encoded), 3);
+}
+
+TEST(SolutionFixture, test_3) {
+    string encoded = "121";
+    ASSERT_EQ(count_decodings(encoded), 3);
+}
+
+TEST(SolutionFixture, test_4) {
+    string encoded = "1234";
+    ASSERT_EQ(count_decodings(encoded), 3);
+}
+
+TEST(SolutionFixture, test_5) {
+    string encoded = "1234421211236";
+    ASSERT_EQ(count_decodings(encoded), 63);
+}
