3cd8401478c9f9a731ca33b7bb116534b8651af1
Akash Kwatra <akashkw@gmail.com>
2019-04-22 01:25:59 -0500

    fixed bugs in optimized solutiong

--- daily_coding_problem/8_count_unival_subtrees.cpp
+++ daily_coding_problem/8_count_unival_subtrees.cpp
@@ -61,14 +61,14 @@ int count_unival_subtrees(const node<int>* const &root) {
 pair<int, bool> get_unival_data(const node<int>* const &root, const int &val) {
     if(!root) 
         return make_pair(0, true);
+    bool maintains_unival = root->data == val;
     if(!root->left && !root->right)
-        return make_pair(1, true);
-    bool maintains_unival = true;
+        return make_pair(1, maintains_unival);
     if((root->left && root->left->data != val) || (root->right && root->right->data != val))
         maintains_unival = false;
     pair<int, bool> left_data = get_unival_data(root->left, root->data);
     pair<int, bool> right_data = get_unival_data(root->right, root->data);
-    int count = left_data.first = right_data.first;
+    int count = left_data.first + right_data.first;
     if(left_data.second && right_data.second)
         count += 1;
     return make_pair(count, maintains_unival);
@@ -121,7 +121,7 @@ TEST(SolutionFixture, test_5) {
     root->left->left = new node<int>(5);
     root->left->right = new node<int>(5);
     root->right->right = new node<int>(5);
-    ASSERT_EQ(count_unival_subtrees_opt(root), 4);
+    ASSERT_EQ(count_unival_subtrees(root), 4);
     delete root;
 }
 
