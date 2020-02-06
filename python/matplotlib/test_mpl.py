f25329519a25bd2be536d6689b6ef1f114e99c13
Akash Kwatra <akashkw@gmail.com>
2020-02-05 18:52:50 -0600

    two lines

--- python/matplotlib/test_mpl.py
+++ python/matplotlib/test_mpl.py
@@ -4,10 +4,15 @@ from random import randint
 if __name__ == '__main__':
     dev_x = list(range(50))
     dev_y = [x + randint(20, 30) for x in dev_x]
+    dev_y2 = [x + randint(10, 20) for x in dev_x]
 
-    plt.plot(dev_x, dev_y)
+    plt.plot(dev_x, dev_y, label='Akash1')
+    plt.plot(dev_x, dev_y2, label='Akash2')
+
     plt.xlabel("Height (inches)")
     plt.ylabel("Age (Years)")
     plt.title("Akash's Height")
+
+    plt.legend()
 
     plt.show()
